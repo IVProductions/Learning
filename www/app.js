b79d2a97f6937a2405302a38d726910f054eab57
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2014-03-10 17:08:53 -0700

    adsa

---
+++ www/app.js
@@ -0,0 +1,24 @@
+
+var Learning = angular.module("Learning", ["ngRoute"]).run(function() {
+    FastClick.attach(document.body);
+}); //add ngRoute ($routeProvider) as a dependency for the app, and init fastclick on body
+
+
+<!--Routing-->
+Learning.config(function ($routeProvider){
+    $routeProvider.when("/", {
+        templateUrl:"partials/splash.html",
+        controller:"mainCtrl"
+    }).when("/main",{
+            templateUrl:"partials/main.html",
+            controller:"mainCtrl"
+        }).when("/splash",{
+            templateUrl:"partials/splash.html",
+            controller:"mainCtrl"
+        }).when("/word",{
+            templateUrl:"partials/word.html",
+            controller:"wordCtrl"
+        }).otherwise({
+            redirectTo:"/"
+        })
+});
---
+++ www/app/services/stateService.js
@@ -0,0 +1,15 @@
+Learning.factory("stateService", function() {
+    var currentWordID = 0;
+
+    var functions = {};
+
+    functions.getCurrentWordID = function() {
+        return currentWordID;
+    }
+    functions.setCurrentWordID = function(wordID) {
+        currentWordID = wordID;
+    }
+    return {
+        functions: functions
+    }
+})
