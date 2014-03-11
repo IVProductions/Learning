1fc52a77fdcfb2e0356aa2a1dffa57cc7990925b
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2014-03-10 17:11:41 -0700

    asdas

--- www/app/app.js
+++ www/app/app.js
@@ -1,22 +1,22 @@
 
-var Ping = angular.module("Ping", ["ngRoute"]).run(function() {
+var Learning = angular.module("Learning", ["ngRoute"]).run(function() {
     FastClick.attach(document.body);
 }); //add ngRoute ($routeProvider) as a dependency for the app, and init fastclick on body
 
 
 <!--Routing-->
-Ping.config(function ($routeProvider){
+Learning.config(function ($routeProvider){
     $routeProvider.when("/", {
-        templateUrl:"partials/splash.html",
+        templateUrl:"app/partials/splash.html",
         controller:"mainCtrl"
     }).when("/main",{
-            templateUrl:"partials/main.html",
+            templateUrl:"app/partials/main.html",
             controller:"mainCtrl"
         }).when("/splash",{
-            templateUrl:"partials/splash.html",
+            templateUrl:"app/partials/splash.html",
             controller:"mainCtrl"
         }).when("/word",{
-            templateUrl:"partials/word.html",
+            templateUrl:"app/partials/word.html",
             controller:"wordCtrl"
         }).otherwise({
             redirectTo:"/"
